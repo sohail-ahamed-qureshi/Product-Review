a21d2731850e4aa4503179750e31f47eb33c2e4f
sohail <sohailqureshi82@gmail.com>
2021-07-06 18:36:25 +0530

    [sohail] Add: Product Review management using LINQ

---
+++ ProductReviewManagementUsingLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReviewManagementUsingLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Product Review Management Using LINQ");
+        }
+    }
+}
