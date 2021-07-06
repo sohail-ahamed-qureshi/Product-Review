59d84a8e5bccdb06a32f1a4282b62ef39d3ea959
sohail <sohailqureshi82@gmail.com>
2021-07-06 20:24:19 +0530

    [sohail] Add: 25 new product reviews added to the list

---
+++ ProductReviewManagementUsingLINQ/ProductReview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagementUsingLINQ
+{
+    class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+        public bool isLike { get; set; }
+    }
+}
--- ProductReviewManagementUsingLINQ/Program.cs
+++ ProductReviewManagementUsingLINQ/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ProductReviewManagementUsingLINQ
 {
@@ -7,6 +8,37 @@ namespace ProductReviewManagementUsingLINQ
         static void Main(string[] args)
         {
             Console.WriteLine("Product Review Management Using LINQ");
+            List<ProductReview> list = new List<ProductReview>
+            {
+                new ProductReview(){ProductID = 1, UserID = 1, Rating =3, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 2, UserID = 2, Rating =2, Review ="Good", isLike = false },
+                new ProductReview(){ProductID = 3, UserID = 3, Rating =1, Review ="Good", isLike = false },
+                new ProductReview(){ProductID = 4, UserID = 4, Rating =5, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 5, UserID = 5, Rating =0, Review ="Good", isLike = false },
+                new ProductReview(){ProductID = 6, UserID = 6, Rating =7, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 7, UserID = 6, Rating =10, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 8, UserID = 6, Rating =9, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 9, UserID = 6, Rating =8, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 10, UserID = 6, Rating =2, Review ="Good", isLike = false },
+                new ProductReview(){ProductID = 11, UserID = 6, Rating =5, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 12, UserID = 6, Rating =9, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 13, UserID = 6, Rating =1, Review ="Good", isLike = false },
+                new ProductReview(){ProductID = 14, UserID = 6, Rating =8, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 15, UserID = 6, Rating =3, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 16, UserID = 6, Rating =7, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 17, UserID = 6, Rating =4, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 18, UserID = 6, Rating =2, Review ="Good", isLike = false },
+                new ProductReview(){ProductID = 19, UserID = 6, Rating =3, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 20, UserID = 6, Rating =1, Review ="Good", isLike = false },
+                new ProductReview(){ProductID = 21, UserID = 6, Rating =10, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 22, UserID = 6, Rating =7, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 23, UserID = 6, Rating =8, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 24, UserID = 6, Rating =4, Review ="Good", isLike = true },
+                new ProductReview(){ProductID = 25, UserID = 6, Rating =2, Review ="Good", isLike = false },
+            };
+
+
+
         }
     }
 }
